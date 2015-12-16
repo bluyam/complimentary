2d031b3e15eec5a70d5ed80aa4142bdb93202638
bluyam <bluyam@tamu.edu>
2015-12-16 15:37:11 -0600

    add blank xcode project

---
+++ compliments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  compliments
+//
+//  Created by Kyle Wilson on 12/16/15.
+//  Copyright © 2015 Bluyam Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
